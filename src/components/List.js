85760864a00e2af20d4271fbef37ccd1594f39d7
Nicole Eric Cabunilas <necabunilas@gmail.com>
2020-08-01 12:09:12 +0800

    fixed receiving of messages for product duplicates

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import socketIOClient from 'socket.io-client';
 const ENDPOINT = 'http://localhost:5000';
 
--- src/components/TableEntries.js
+++ src/components/TableEntries.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Animated } from 'react-animated-css';
 import { StartCase } from 'react-lodash';
 import List from './List';
