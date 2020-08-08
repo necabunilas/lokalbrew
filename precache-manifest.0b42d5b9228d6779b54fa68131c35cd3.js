d9ea45689e75823cd02b5e626071ba9e2bd96001
Nicole Eric Cabunilas <necabunilas@gmail.com>
2020-08-08 12:02:02 +0800

    Updates

---
+++ precache-manifest.0b42d5b9228d6779b54fa68131c35cd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c25a22603ac3410381853318cb51019",
+    "url": "/lokalbrew/index.html"
+  },
+  {
+    "revision": "41f41c0abbfe62c2eb7e",
+    "url": "/lokalbrew/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "ca84406d80d7b2e435b5",
+    "url": "/lokalbrew/static/css/main.a81240c6.chunk.css"
+  },
+  {
+    "revision": "41f41c0abbfe62c2eb7e",
+    "url": "/lokalbrew/static/js/2.7cb8403e.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/lokalbrew/static/js/2.7cb8403e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca84406d80d7b2e435b5",
+    "url": "/lokalbrew/static/js/main.b387d246.chunk.js"
+  },
+  {
+    "revision": "f5c0874f6491aaf29bbc",
+    "url": "/lokalbrew/static/js/runtime-main.0c5f14e0.js"
+  }
+]);
